11c00f29614acaa0a9347f79f07e718fa159d75e
Anna Ulanova <anetteua0@gmail.com>
2017-07-23 00:52:55 -0700

    goes through deck onclick

---
+++ app.js
@@ -0,0 +1,25 @@
+function imageClicked() {
+  var images = document.querySelectorAll("img");
+  var curIndex = 0;
+
+  for (var i = 0; i < images.length; i++) {
+    if (images[i].style.display !== "none") {
+      curIndex = i;
+      break;
+    }
+  }
+  if (curIndex >= images.length - 1) {
+    return;
+  }
+
+  images[curIndex].style.display = "none";
+  images[curIndex + 1].style.display = "block";
+}
+
+window.onload = function() {
+  var images = document.querySelectorAll("img");
+
+  for (var i = 0; i < images.length; i++) {
+    images[i].onclick = imageClicked;
+  }
+}
